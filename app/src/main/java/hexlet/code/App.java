9a487c8467d4254a75f76ed6db9ec99d3899b147
MAG <ivan_ivanov576@icloud.com>
2023-11-03 20:15:39 +0300

    modified build.gradle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,6 @@
-package hexlet.cod;
+package hexlet.code;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
